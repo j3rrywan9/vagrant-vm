a609a7fb5a15de04b73e90ba76570b2c1ff3c4f4
Jerry Wang <jerry.wang@delphix.com>
2015-07-11 18:12:04 -0700

    remove tools/vagrant/provision.pp

--- tools/vagrant/provision.sh
+++ tools/vagrant/provision.sh
@@ -19,30 +19,28 @@ function warn
     echo "WARNING: $*" >&2
 }
 
-function install_puppet_module
-{
-    local module=$1
-
-    if ! puppet module --modulepath=/etc/puppet/modules list | \
-        grep $module >/dev/null; then
-        sudo puppet module install $module || \
-            error "failed to install puppet module ${module}"
-    fi
-}
-
 # Ensure that we have an internet connection by pinging Google DNS servers.
 ping -w10 -c1 8.8.8.8 >/dev/null 2>&1 \
     || fatal "This VM is not connected to the internet."
 
-install_puppet_module "puppetlabs-apt"
+sudo apt-get install software-properties-common
+if [[ $? > 0 ]]; then
+	error "failed to install software-properties-common."
+fi
 
-sudo puppet apply --detailed-exitcodes /vagrant/tools/vagrant/provision.pp
-puppet_exit=$?
+sudo apt-add-repository ppa:ansible/ansible
+if [[ $? > 0 ]]; then
+	error "failed to install Ansible PPA."
+fi
 
-#
-# Puppet will exit 0 if there were no changes or errors, 2 if there were
-# changes and no errors, and 4 or 6 if there were errors.
-#
-[[ $puppet_exit -eq 0 ]] || [[ $puppet_exit -eq 2 ]] || error "puppet failed"
+sudo apt-get update
+if [[ $? > 0 ]]; then
+	error "failed to run apt-get update."
+fi
+
+sudo apt-get install ansible
+if [[ $? > 0 ]]; then
+	error "failed to install Ansible."
+fi
 	
 exit 0
